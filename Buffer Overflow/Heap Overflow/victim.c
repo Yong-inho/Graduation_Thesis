fc5ce4f94b0a40d4584fa86e8b4a0e84a8ec56b6
Yong-inho <62104730+Yong-inho@users.noreply.github.com>
2020-04-21 21:27:23 +0900

    .
    .

---
+++ Buffer Overflow/Heap Overflow/victim.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]) {
+  int fd;
+  char* buffer, *datafile;
+  
+  buffer = (char*)malloc(100);
+  datafile = (char*)malloc(20);
+  strcpy(datafile, "/etc/notes");
+  
+  if(argc < 2)
+    printf("Usage : %s <data to add to %s>\n", argv[0], datafile);
+  
+  strcpy(buffer, argv[1]);
+  
+  fd = open(datafile, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+  if(fd == -1)
+    perror("in main() open()");
+  
+  if(write(fd, buffer, strlen(buffer)) == -1)
+    perror("in main() write()");
+  write(fd, "\n", 1);
+  
+  free(buffer);
+  free(datafile);
+  return 0;
+}
